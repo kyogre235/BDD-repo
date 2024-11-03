30e08800b5b37517fb74714bb07bac066047b2a4
Kyogre235 <jacomedelgadoa@ciencias.unam.mx>
2024-11-03 15:08:11 -0600

    faltan queries

--- sql/Query.sql
+++ sql/Query.sql
@@ -1,20 +1,39 @@
--- Querys Practica07
-
--- Selecciona todos los renglones de atleta que cumplan que su nombre empiece con R o r.
-select * from atleta
-where nombre ~ '^R'
-
--- Selecciona todos los renglones de entrenador cuyo mes de su fecha de nacimiento sea el 6 (E,F,M,A,M,Junio).
-select * from entrenador
-where extract (month from fechanacimiento) = 6;
-
--- Selecciona todos los renglones de evento tal que sean mayores o iguales al 1-enero-2024 pero menores o iguales al 14-abril-2024.
-select * from evento
-where fecha >= '2024-01-01' AND fecha <= '2024-04-14';
-
--- Selecciona todos los renglones de localidad tal que su aforo es mayor a 400.
-select * from localidad
-where aforo > 400;
-
--- Selecciona todos los diferentes patrocinadores registrados en patrocinador y muestra solo el nombre.
-select distinct patrocinador from patrocinador
+-- Query #1
+-- Query #2
+select * from evento where precioinic > '2500.00' order by precioinic 
+-- Query #3
+select a.idolimpicoa as atleta, a.nombre, a.apellidopaterno
+from participaratleta p 
+join atleta a on p.idolimpicoa = a.idolimpicoa
+join evento e on p.idevento = e.idevento 
+group by a.idolimpicoa
+having count(*) > 1; 
+--Query #4
+--Query #5
+select patrocinador from patrocinador
+group by patrocinador 
+having count(*) < 2;
+--Query #6
+select count(*) as medallasOro 
+from medalla m 
+join atleta a on m.idolimpicoa = a.idolimpicoa
+where a.triclave = 'MEX' and m.lugar = 1;
+--Query #7
+select count(*) as medallasPlata 
+from medalla m 
+join atleta a on m.idolimpicoa = a.idolimpicoa
+where a.triclave = 'JPN' and m.lugar = 2 ;
+--Query #8
+select count(*) as medallasBronce 
+from medalla m 
+join atleta a on m.idolimpicoa = a.idolimpicoa
+where a.triclave = 'ESP' and m.lugar = 3;
+--Query #9
+select a.idolimpicoa as idatleta, a.idolimpicoe as identrenador,a.triclave as pais, 
+a.nombre as nombre, a.apellidopaterno as apellido1, a.apellidomaterno as apellido2, 
+a.fechanacimiento as cumple, a.genero as genero
+from medalla m 
+join disciplina d on m.iddisciplina = d.iddisciplina 
+join atleta a  on m.idolimpicoa = a.idolimpicoa 
+where d.nombre ='halterofilia'
+--Query #10
