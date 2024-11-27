0cf22116d488383ec83f66197cb9eef1732a8537
Kyogre235 <jacomedelgadoa@ciencias.unam.mx>
2024-11-27 15:14:04 -0600

    queries de emma

--- sql/Query.sql
+++ sql/Query.sql
@@ -1,123 +1,43 @@
 -- Query #1
-drop table if exists disa;
-create temporary table if not exists disa as
-select a.idolimpicoa, a.nombre as nombrea, a.apellidopaterno as apellidoa, t.iddisciplina as disa
-from atleta a
-natural join trabajaratleta t;
+--Saber cuantas mujeres ganaron medallas de oro donde están ordenadas de la más joven a la más vieja.
 
-drop table if exists dise;
-create temporary table if not exists dise as
-select e.idolimpicoe, e.nombre as nombree, e.apellidopaterno as apellidoe, t.iddisciplina as dise
-from entrenador e
-natural join trabajaratleta t;
+SELECT *
+FROM Atleta
+INNER JOIN Medalla
+ON Atleta.idOlimpicoA = Medalla.idOlimpicoA
+where Atleta.Genero='F' and Medalla.Lugar=1
+ORDER BY Atleta.FechaNacimiento desc;
 
-drop table if exists res;
-create temporary table if not exists res as
-select * 
-from disa da
-join dise de on da.disa = de.dise and da.apellidoa = de.apellidoe;
+--Query #2
+--¿Cuál es la disciplina con más atletas participando y de que pais proviene la mayoría de estos atletas?
 
-select r.idolimpicoa as id, r.nombrea as nombre,r.apellidoa as paterno  
-from res r
-union 
-select r.idolimpicoe as id, r.nombree as nombre, r.apellidoe as paterno
-from res r
-order by paterno;
+SELECT 
+    Disciplina.nombre AS Disciplina,
+    COUNT(Atleta.idOlimpicoA) AS TotalAtletas,
+    Pais.Nombre AS PaisMayorParticipacion
+FROM 
+    Atleta
+INNER JOIN Pais
+    ON Pais.TRICLAVE = Atleta.TRICLAVE
+INNER JOIN TrabajarAtleta
+    ON Atleta.idOlimpicoA = TrabajarAtleta.IdOlimpicoA
+INNER JOIN Disciplina
+    ON Disciplina.idDisciplina = TrabajarAtleta.IdDisciplina
+GROUP BY 
+    Disciplina.nombre, Pais.Nombre
+ORDER BY 
+    TotalAtletas DESC
+LIMIT 1;
 
--- Query #2
-select * from evento where precioinic > '2500.00' order by precioinic 
+--Query #3
+--nombre de los entrenadores entrenaron a los atletas con medallas olímpicas y cuantas medallas ganaron los atletas
 
--- Query #3
-select a.idolimpicoa as atleta, a.nombre, a.apellidopaterno, count(*) as disciplinas 
-from trabajaratleta p 
-join atleta a on p.idolimpicoa = a.idolimpicoa
-join disciplina e on p.iddisciplina = e.iddisciplina 
-group by a.idolimpicoa
-having count(*) > 1; 
-
---Query #4
-drop table if exists eventoe;
-create temporary table if not exists eventoe as
-select e.idolimpicoe, e.triclave as paise, e.nombre as nombreE, e.apellidopaterno as apellidopaternoE , p.idevento as eventoe
-from entrenador e
-natural join participarentrenador p;
-
-drop table if exists eventoj;
-create temporary table if not exists eventoj as
-select j.idolimpicoj, j.triclave as paisj, j.nombre as nombreJ, j.apellidopaterno as apellidopaternoJ, p.idevento as eventoj
-from juez j
-natural join participarjuez p;
-
-drop table if exists diff;
-create temporary table if not exists diff as
-select * 
-from eventoe e
-join eventoj j on e.eventoe != j.eventoj and e.paise = j.paisj;
-
-select d.idolimpicoe as id, d.nombree as nombre, d.paise as pais, d.eventoe as evento
-from diff d
-union
-select  d.idolimpicoj as id, d.nombrej as nombre, d.paisj as pais, d.eventoj as evento
-from diff d;
-
---Query #5
-select patrocinador from patrocinador
-group by patrocinador 
-having count(*) < 2;
-
---Query #6
-select count(*) as medallasOro 
-from medalla m 
-join atleta a on m.idolimpicoa = a.idolimpicoa
-where a.triclave = 'MEX' and m.lugar = 1;
-
---Query #7
-select count(*) as medallasPlata 
-from medalla m 
-join atleta a on m.idolimpicoa = a.idolimpicoa
-where a.triclave = 'JPN' and m.lugar = 2 ;
-
---Query #8
-select count(*) as medallasBronce 
-from medalla m 
-join atleta a on m.idolimpicoa = a.idolimpicoa
-where a.triclave = 'ESP' and m.lugar = 3;
-
---Query #9
-select a.idolimpicoa as idatleta, a.idolimpicoe as identrenador,a.triclave as pais, 
-a.nombre as nombre, a.apellidopaterno as apellido1, a.apellidomaterno as apellido2, 
-a.fechanacimiento as cumple, a.genero as genero
-from medalla m 
-join disciplina d on m.iddisciplina = d.iddisciplina 
-join atleta a  on m.idolimpicoa = a.idolimpicoa 
-where d.nombre ='halterofilia';
-
---Query #10
-DROP TABLE IF EXISTS Lugar1;
-CREATE TEMPORARY TABLE IF NOT EXISTS Lugar1 AS
-SELECT m.idolimpicoa, SUM(m.lugar) AS Oro
-FROM Medalla m 
-WHERE m.lugar = 1 
-GROUP BY m.idolimpicoa;
-
-DROP TABLE IF EXISTS Lugar2;
-CREATE TEMPORARY TABLE IF NOT EXISTS Lugar2 AS
-SELECT m.idolimpicoa, SUM(m.lugar) AS Plata
-FROM Medalla m 
-WHERE m.lugar = 2
-GROUP BY m.idolimpicoa;
-
-DROP TABLE IF EXISTS Lugar3;
-CREATE TEMPORARY TABLE IF NOT EXISTS Lugar3 AS
-SELECT m.idolimpicoa, SUM(m.lugar) AS Bronce
-FROM Medalla m 
-WHERE m.lugar = 3
-GROUP BY m.idolimpicoa;
-
-SELECT a.idolimpicoa, a.idolimpicoe, a.triclave, a.nombre, a.apellidopaterno, a.apellidomaterno, a.fechanacimiento, a.genero, COALESCE(l1.oro, 0) as oro, COALESCE(l2.plata, 0) as plata, COALESCE(l3.bronce, 0) as bronce FROM Atleta a 
-LEFT JOIN Lugar1 l1 ON a.idolimpicoa = l1.idolimpicoa
-LEFT JOIN Lugar2 l2 ON a.idolimpicoa = l2.idolimpicoa
-LEFT JOIN Lugar3 l3 ON a.idolimpicoa = l3.idolimpicoa
-where l1.oro != 0 or l2.plata != 0 or l3.bronce != 0
-order by l1.oro desc nulls last,l2.plata desc nulls last,l3.bronce desc nulls last;
-
+select Entrenador.Nombre,Entrenador.ApellidoPaterno,Entrenador.ApellidoMaterno,count(Medalla.Lugar) as TotalMedallas
+from atleta
+inner join Entrenador
+	on Entrenador.idOlimpicoE = Atleta.idOlimpicoE
+inner join Medalla
+	on Medalla.idOlimpicoA = Atleta.idOlimpicoA
+where Medalla.Lugar = 1
+group by Entrenador.idolimpicoe 
+order by TotalMedallas desc;
